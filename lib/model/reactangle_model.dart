fe3f23faa3078534996bc67f40fdc34897696d80
Beginner-Project <boripats63@nu.ac.th>
2023-10-07 23:03:29 +0700

    feat(model): create rectangle model

---
+++ lib/model/reactangle_model.dart
@@ -0,0 +1,12 @@
+class RectangleModel {
+  late double left, top, width, height;
+  RectangleModel(
+      {required this.height,
+      required this.width,
+      required this.left,
+      required this.top});
+
+  Map<String, double> toJson() {
+    return {"left": left, "top": top, "height": height, "width": width};
+  }
+}
