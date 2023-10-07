4364b96c847270315ae103f9de345c13352b0bab
Beginner-Project <boripats63@nu.ac.th>
2023-10-07 22:59:53 +0700

    feat(api): create blocker capture api call

---
+++ lib/api/blocker_capture_request.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:blocker_translator/model/index.dart';
+import 'package:http/http.dart' as http;
+
+void blockerCaptureRequest(RectangleModel rectangleModel) async {
+  Map<String, String> headers = {
+    'Content-Type': 'application/json', // You can set other headers as needed
+  };
+  final url = Uri.parse("http://127.0.0.1:8000/blocker_capture");
+  final response = await http.post(url,
+      headers: headers, body: jsonEncode(rectangleModel.toJson()));
+  if (response.statusCode != 200) {
+    throw Exception(
+        "Failed to fetch blockerCaptureRequest : ${response.statusCode}");
+  }
+}
