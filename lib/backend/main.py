3f8614337ef5312481c31e19a3cdeb9b2dd8c317
Beginner-Project <boripats63@nu.ac.th>
2023-10-07 23:02:58 +0700

    feat(backend): update api and add logic
    logic: blocker_capture
    api: set_blocker_capture,blocker_capture_feed

--- lib/backend/main.py
+++ lib/backend/main.py
@@ -1,16 +1,26 @@
+from mss import mss
 import uvicorn
 from fastapi import FastAPI, responses
 import ctypes
 from PIL import Image
 import numpy as np
 from pydantic import BaseModel
-import cv2 
+import cv2
+
 
 class Rectangle(BaseModel):
     left:  int
     top: int
     width: int
     height: int
+
+    def toDict(self):
+        return {
+            "left": self.left,
+            "top": self.top,
+            "width": self.width,
+            "height": self.height
+        }
 
 
 class Server():
@@ -21,26 +31,38 @@ class Server():
         user32 = ctypes.windll.user32
         self.screensize = user32.GetSystemMetrics(
             0), user32.GetSystemMetrics(1)
-    
+
     def get_frame(self):
         frame = np.random.randint(
-        low=0, high=255, size=(480, 640, 3), dtype='uint8')
+            low=0, high=255, size=(480, 640, 3), dtype='uint8')
         encodedImage = cv2.imencode('.png', frame)[1]
         yield (encodedImage.tobytes())
-    
+
+    def blocker_capture(self):
+        mons_res = self.store_rectangle.toDict()
+        sct = mss()
+        screenshot = sct.grab(mons_res)
+        img = Image.frombytes(
+            "RGB", (screenshot.width, screenshot.height), screenshot.rgb)
+        array_image = np.array(img, dtype="uint8")
+        array_image = np.flip(array_image, axis=2)
+        encodedImage = cv2.imencode(".png", array_image)[1]
+        yield (encodedImage.tobytes())
+
     def run(self):
         @self.app.post("/blocker_capture")
-        async def set_blocker_capture():
-            return 'hello'
+        async def set_blocker_capture(rectangle: Rectangle):
+            self.store_rectangle = rectangle
+            return self.store_rectangle.toDict()
 
         @self.app.get("/blocker_capture")
         async def blocker_capture_feed():
-            return "hello"
+            return responses.StreamingResponse(self.blocker_capture())
 
         @self.app.get("/frame")
         async def frame():
             return responses.StreamingResponse(self.get_frame())
-        
+
         @self.app.get("/")
         async def root():
             self.count += 1
