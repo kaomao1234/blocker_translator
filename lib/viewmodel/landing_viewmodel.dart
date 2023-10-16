89ef2a48530581f21b73a8e7ae5c27394b740699
Beginner-Project <boripats63@nu.ac.th>
2023-10-16 17:34:23 +0700

    feat(viewmodel) := Creates a callback parameter to pass values ​​into the view.

--- lib/viewmodel/landing_viewmodel.dart
+++ lib/viewmodel/landing_viewmodel.dart
@@ -14,6 +14,9 @@ class LandingViewModel with ChangeNotifier {
   Uint8List? imageBytes;
   int titleBarHeight = 0;
   Timer? prevTimer;
+  int frameCounter = 0;
+  int lastTime = DateTime.now().millisecondsSinceEpoch;
+  int fps = 0;
   void calcblockerSize() async {
     titleBarHeight = await windowManager.getTitleBarHeight();
     Size size = await windowManager.getSize();
@@ -22,6 +25,8 @@ class LandingViewModel with ChangeNotifier {
     calcRect();
     notifyListeners();
   }
+
+  RectangleModel? get rectangleModel => _rectangleModel;
 
   void calcRect() async {
     Rect windowBound = await windowManager.getBounds();
@@ -32,19 +37,40 @@ class LandingViewModel with ChangeNotifier {
         top: windowBound.top + titleBarHeight + 59);
   }
 
-  void repeatingCallFrame(bool allowToGet) async {
+  void repeatingCallFrame(
+      bool allowToGet, void Function(Uint8List? imageBytes) callback) async {
     prevTimer?.cancel();
-    prevTimer = Timer.periodic(const Duration(seconds: 2), (timer) async {
+    prevTimer =
+        Timer.periodic(const Duration(milliseconds: 100), (timer) async {
       imageBytes = null;
-      notifyListeners();
+      callback(imageBytes);
       if (allowToGet) {
         blockerCaptureRequest(_rectangleModel!);
         imageBytes = await blockerCaptureFeed();
+        String? textCaptured = await textFromImage();
+        log(textCaptured.toString());
       } else {
         imageBytes = null;
         timer.cancel();
       }
-      notifyListeners();
+      callback(imageBytes);
     });
+  }
+
+  Future<String> callTextFromImage() async {
+    return await textFromImage() ?? "";
+  }
+
+  Future<Uint8List> callBlockerCaptureFeed() async {
+    blockerCaptureRequest(_rectangleModel!);
+    return await blockerCaptureFeed() ?? Uint8List(0);
+  }
+
+  Future<Uint8List> callFrame() async {
+    return await frame() ?? Uint8List(0);
+  }
+
+  bool timeDifferenceBiggerThanSecond() {
+    return DateTime.now().millisecondsSinceEpoch - lastTime > 1000;
   }
 }
