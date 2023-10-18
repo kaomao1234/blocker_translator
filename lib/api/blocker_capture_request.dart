627f735aebdece7a11c322888f15821208561367
Beginner-Project <boripats63@nu.ac.th>
2023-10-18 16:30:14 +0700

    feat(api):= change param type and model

--- lib/api/blocker_capture_request.dart
+++ lib/api/blocker_capture_request.dart
@@ -2,13 +2,13 @@ import 'dart:convert';
 import 'package:blocker_translator/model/index.dart';
 import 'package:http/http.dart' as http;
 
-void blockerCaptureRequest(RectangleModel rectangleModel) async {
+void blockerCaptureRequest(BlockerModel _blockerModel) async {
   Map<String, String> headers = {
     'Content-Type': 'application/json', // You can set other headers as needed
   };
   final url = Uri.parse("http://127.0.0.1:8000/blocker_capture");
   final response = await http.post(url,
-      headers: headers, body: jsonEncode(rectangleModel.toJson()));
+      headers: headers, body: jsonEncode(_blockerModel.toJson()));
   if (response.statusCode != 200) {
     throw Exception(
         "Failed to fetch blockerCaptureRequest : ${response.statusCode}");
