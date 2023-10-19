2fc56fdc3b89724b2085b13d4a4f5b369687ae9c
Beginner-Project <boripats63@nu.ac.th>
2023-10-19 17:32:08 +0700

    feat(hooks):= create region_box_state

---
+++ lib/hooks/region_box.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RegionBoxState {
+  bool isEdit = false;
+  double top = 0, left = 0, width = 150, height = 100;
+  TextEditingController controller = TextEditingController();
+  RegionBoxState();
+}
