7f1d53265a21d6a6651ca7d2d19941493ada71bf
Beginner-Project <boripats63@nu.ac.th>
2023-10-20 18:02:56 +0700

    feat(widget):= add region box

--- lib/widget/region_box.dart
+++ lib/widget/region_box.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:blocker_translator/hooks/region_box.dart';
 import 'package:flutter/material.dart';
 
 class RegionBox extends StatefulWidget {
   late RegionBoxState state;
-  void Function(DragUpdateDetails details) boxPositionUpdate, boxSizeUpdate;
+  void Function(DragUpdateDetails details, RegionBoxState state)
+      boxPositionUpdate, boxSizeUpdate;
   RegionBox(
       {super.key,
       required this.state,
@@ -16,6 +19,32 @@ class RegionBox extends StatefulWidget {
 
 class _RegionBoxState extends State<RegionBox> {
   late RegionBoxState state = widget.state;
+  bool isEnter = false;
+
+  GestureDetector _gestureDetector() {
+    return GestureDetector(
+        onPanUpdate: (details) => widget.boxPositionUpdate(details, state));
+  }
+
+  Column _column() {
+    return Column(
+      children: [
+        Text(
+          state.name,
+          style: TextStyle(fontSize: 16),
+        ),
+        TextField(
+          decoration: InputDecoration(hintText: "Input box name"),
+          controller: state.controller,
+          onChanged: (value) {
+            setState(() {
+              state.name = value;
+            });
+          },
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,38 +53,67 @@ class _RegionBoxState extends State<RegionBox> {
         top: state.top,
         child: Container(
           width: state.width,
-          height: state.height,
-          color: Colors.blue,
-          child: Column(
-            children: [
-              Expanded(
-                  child:
-                      GestureDetector(onPanUpdate: widget.boxPositionUpdate)),
-              SizedBox(
-                height: 18,
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: GestureDetector(
-                    onPanUpdate: widget.boxSizeUpdate,
-                    child: Container(
-                      width: 18.0,
-                      height: 18.0,
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.crop,
-                          size: 18,
-                          color: Colors.white,
+          height: state.height + 20,
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            onEnter: (pointerEnter) {
+              setState(() {
+                isEnter = true;
+              });
+            },
+            onExit: (pointerExit) {
+              setState(() {
+                isEnter = false;
+              });
+            },
+            child: Column(
+              children: [
+                Container(
+                  height: state.height,
+                  width: state.width,
+                  decoration: BoxDecoration(
+                      color: state.isEdit ? Colors.white : Colors.transparent,
+                      border: Border.all(
+                          color: isEnter ? Colors.green : Colors.blue,
+                          width: 2)),
+                  child: state.isEdit ? _column() : _gestureDetector(),
+                ),
+                if (state.isEdit)
+                  SizedBox(
+                    height: 20,
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        color: Colors.amberAccent,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              width: 18.0,
+                              height: 18.0,
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                shape: BoxShape.circle,
+                              ),
+                              child: GestureDetector(
+                                onPanUpdate: (details) =>
+                                    widget.boxSizeUpdate(details, state),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.crop,
+                                    size: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                  ),
-                ),
-              )
-            ],
+                  )
+              ],
+            ),
           ),
         ));
   }
