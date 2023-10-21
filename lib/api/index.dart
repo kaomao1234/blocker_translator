1f67f030362f25fc2789d4019bf4a4f6ff8109e2
Beginner-Project <boripats63@nu.ac.th>
2023-10-21 17:58:23 +0700

    Update index.dart

--- lib/api/index.dart
+++ lib/api/index.dart
@@ -1,6 +1,7 @@
-export 'package:blocker_translator/api/text_from_image.dart';
+export 'package:blocker_translator/api/region_detector.dart';
+export 'package:blocker_translator/api/region_detector_request.dart';
 export 'package:blocker_translator/api/blocker_capture_request.dart';
-export 'package:blocker_translator/api/blocker_capture_feed.dart';
+export 'package:blocker_translator/api/blocker_detector.dart';
 export 'package:blocker_translator/api/frame.dart';
 
 
