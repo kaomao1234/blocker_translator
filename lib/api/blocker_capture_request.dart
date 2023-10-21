8f513b5d7764a0d2051991246228a67e482435b7
Beginner-Project <boripats63@nu.ac.th>
2023-10-21 17:58:13 +0700

    Update blocker_capture_request.dart

--- lib/api/blocker_capture_request.dart
+++ lib/api/blocker_capture_request.dart
@@ -2,13 +2,13 @@ import 'dart:convert';
 import 'package:blocker_translator/model/index.dart';
 import 'package:http/http.dart' as http;
 
-void blockerCaptureRequest(BlockerModel _blockerModel) async {
+void blockerDetectorRequest(BlockerModel blockerModel) async {
   Map<String, String> headers = {
     'Content-Type': 'application/json', // You can set other headers as needed
   };
-  final url = Uri.parse("http://127.0.0.1:8000/blocker_capture");
+  final url = Uri.parse("http://127.0.0.1:8000/blocker_detector");
   final response = await http.post(url,
-      headers: headers, body: jsonEncode(_blockerModel.toJson()));
+      headers: headers, body: jsonEncode(blockerModel.toJson()));
   if (response.statusCode != 200) {
     throw Exception(
         "Failed to fetch blockerCaptureRequest : ${response.statusCode}");
